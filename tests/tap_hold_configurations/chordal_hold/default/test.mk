dca230a65693e91b1d30dc526d201d09b8aa777a
Benjamin Legrand <benjilegnard@protonmail.com>
2024-01-21 18:05:55 +0100

    feat: add benjilegnard's sweep keymap

--- tests/tap_hold_configurations/chordal_hold/default/test.mk
+++ tests/tap_hold_configurations/chordal_hold/default/test.mk
@@ -1,5 +1,4 @@
-# Copyright 2022 Vladislav Kucheriavykh
-# Copyright 2024 Google LLC
+# Copyright 2022 splitkb.com <support@splitkb.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,4 +13,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-INTROSPECTION_KEYMAP_C = test_keymap.c
+DEFAULT_FOLDER = splitkb/aurora/sweep/rev1
+BOOTLOADER = caterina
