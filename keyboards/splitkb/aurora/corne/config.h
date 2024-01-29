012ec392534f13acbc8b1ec222d47944af63035a
Benjamin Legrand <benjilegnard@protonmail.com>
2024-01-29 20:06:50 +0100

    feat(corne): enable animations and add options in config.h

--- keyboards/splitkb/aurora/corne/config.h
+++ keyboards/splitkb/aurora/corne/config.h
@@ -20,3 +20,32 @@
 #ifdef OLED_ENABLE
 #   define OLED_FONT_H "keyboards/splitkb/aurora/corne/glcdfont.c"
 #endif
+
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 250
+
+#ifndef LED_ANIMATIONS
+#    define LED_ANIMATIONS
+#endif
+
+#undef RGBLIGHT_HUE_STEP
+#define RGBLIGHT_HUE_STEP 4
+
+#if defined(LED_ANIMATIONS)
+    // effects
+    // #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_EFFECT_SNAKE
+    #undef RGBLIGHT_EFFECT_KNIGHT
+    #undef RGBLIGHT_EFFECT_CHRISTMAS
+    #undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #undef RGBLIGHT_EFFECT_RGB_TEST
+    #undef RGBLIGHT_EFFECT_ALTERNATING
+    #define RGBLIGHT_EFFECT_TWINKLE
+    // options
+    #define RGBLIGHT_EFFECT_SNAKE_LENGTH 4    // How many LEDs wide to light up
+
+#endif
+
